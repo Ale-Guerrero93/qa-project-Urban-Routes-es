18340f4d082bb2931096536b80dbd9961eeafc92
Ale-Guerrero93 <kagr93@gmail.com>
2024-11-18 15:58:41 -0800

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-2d0945fb-cedd-4199-bda9-07be70e50844.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-2fc7bb93-1754-4351-be90-65ecf57c7627.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
