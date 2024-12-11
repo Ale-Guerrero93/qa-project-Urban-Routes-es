c5aa89b28b73a6f65555de9261105203729c466c
Ale-Guerrero93 <kagr93@gmail.com>
2024-12-10 16:27:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-from lib2to3.pgen2 import driver
 import data
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 class UrbanRoutesPage:
+
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
     phone_number_field = (By.ID, 'phone')
     card_number_field = (By.ID, 'number')
     card_code_field = (By.ID, 'code')
     message_for_driver_field = (By.ID, 'message')
-    tarifa_comfort_button = (By.ID, 'comfort')
-    manta_button = (By.ID, 'manta')
-    panuelos_button = (By.ID, 'panuelos')
-    helados_button = (By.ID, 'helados')
-    esperar_taxi_field = (By.ID, 'taxi')
-    driver.find_element = (By.NAME, "email")
-    driver.find_element = (By.CLASS_NAME, "form-input")
-    driver.find_element = (By.XPATH, "//input[@type='submit']")
+    comfort_rate_button = (By.ID, 'comfort')
+    blanket_button = (By.ID, 'blanket')
+    handkerchiefs_button = (By.ID, 'handkerchiefs')
+    ice_creams_button = (By.ID, 'ice_creams')
+    taxi_button = (By.ID, 'taxi')
+    driver_info = (By.ID, 'driver_info')
+    model_looking_for_taxi_locator = (By.ID, 'model_taxi_searching')
 
     def __init__(self, driver):
-        self.driver = driver
+            self.driver = driver
 
     def find_email_field(self):
-            # Usando CSS Selector para localizar el campo de email
-        return self.driver.find_element(By.CSS_SELECTOR, "input[name='email']")
+        return self.driver.find_element(By.NAME, "email")
+
+    def find_form_input(self):
+        return self.driver.find_element(By.CLASS_NAME, "form-input")
+
+    def submit_form(self):
+        return self.driver.find_element(By.XPATH, "//input[@type='submit']").click()
 
     def set_from(self, from_address):
         self.driver.find_element(*self.from_field).send_keys(from_address)
@@ -36,43 +39,90 @@ class UrbanRoutesPage:
     def set_to(self, to_address):
         self.driver.find_element(*self.to_field).send_keys(to_address)
 
-    def get_from(self):
+    def get_from_address(self):
         return self.driver.find_element(*self.from_field).get_property('value')
 
-    def get_to(self):
+    def get_to_address(self):
         return self.driver.find_element(*self.to_field).get_property('value')
 
-    def seleccionar_tarifa_comfort(self):
-        comfort = self.driver.find_element(*self.tarifa_comfort_button)
-        comfort.click()
+    def set_route(self, from_address, to_address):
+        self.set_from(from_address)
+        self.set_to(to_address)
+        self.click_taxi_button()
 
-    def rellenar_telefono(self, phone):
-        phone_number = self.driver.find_element(*self.phone_number_field)
-        (phone_number.clear())
-        phone_number.send_keys(phone)
+    def click_taxi_button(self):
+        self.driver.find_element(*self.taxi_button).click()
 
-    def agregar_tarjeta(self, number, code):
-        self.driver.find_element(*self.card_number_field).click()
-        self.driver.find_element(*self.card_number_field).send_keys(number)
-        self.driver.find_element(*self.card_code_field).send_keys(code)
-        self.driver.find_element(*self.card_code_field).send_keys(Keys.TAB)
-        self.driver.find_element(*self.card_code_field).send
+    def get_selected_rate(self):
+        selected_rate = self.driver.find_element(*self.comfort_rate_button)
+        return 'selected' in selected_rate.get_attribute('class')
 
-    def pedir_manta_y_panuelos(self):
-        self.driver.find_element(*self.manta_button).click()
-        self.driver.find_element(*self.panuelos_button).click()
+    def select_comfort_rate(self):
+        self.driver.find_element(*self.comfort_rate_button).click()
+
+    def get_phone_number(self):
+        return self.driver.find_element(*self.phone_number_field).get_property('value')
+
+    def fill_phone_number(self, phone_number):
+        phone_number_field = self.driver.find_element(*self.phone_number_field)
+        phone_number_field.clear()
+        phone_number_field.send_keys(phone_number)
+
+    def get_card_number(self):
+        return self.driver.find_element(*self.card_number_field).get_property('value')
+
+    def add_card(self, number, code):
+        card_number_field = self.driver.find_element(*self.card_number_field)
+        card_number_field.click()
+        card_number_field.send_keys(number)
+
+        card_code_field = self.driver.find_element(*self.card_code_field)
+        card_code_field.send_keys(code)
+
+    def is_blanket_and_handkerchiefs_ordered(self):
+        blanket_class = self.driver.find_element(*self.blanket_button).get_attribute('class')
+        handkerchiefs_class = self.driver.find_element(*self.handkerchiefs_button).get_attribute('class')
+        return 'selected' in blanket_class and 'selected' in handkerchiefs_class
+
+    def ask_for_a_blanket_and_handkerchiefs(self):
+        self.driver.find_element(*self.blanket_button).click()
+        self.driver.find_element(*self.handkerchiefs_button).click()
+
+    def order_ice_cream(self):
+        self.driver.find_element(*self.ice_creams_button).click()
+
+    def is_ice_cream_orders(self):
+        return 'selected' in self.driver.find_element(*self.ice_creams_button).get_attribute('class')
+
+    def get_message_for_driver(self):
+        return self.driver.find_element(*self.message_for_driver_field).get_property('value')
+
+    def message_for_driver(self, message):
+        field = self.driver.find_element(*self.message_for_driver_field)
+        field.clear()
+        field.send_keys(message)
+    def wait_for_element(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    def set_message_for_driver(self, message):
+        self.driver.find_element(*self.message_for_driver_field).send_keys(message)
 
         WebDriverWait(self.driver, 10)
 
-    def esperar_taxi(self):
+    def wait_for_taxi(self):
+        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(self.taxi_button))
 
-        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(self.esperar_taxi()))
+    def wait_model_looking_for_taxi(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.model_looking_for_taxi_locator)
+        )
 
-    def esperar_informacion_conductor(self):
+    def is_model_looking_for_taxi_visible(self):
+        return self.driver.find_element(*self.model_looking_for_taxi_locator).is_displayed()
 
-        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(self.esperar_informacion_conductor()))
-
-
+    def wait_for_driver_information(self):
+        driver_info_locator = (By.ID, 'driver_info')  # Cambia según tu HTML
+        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(driver_info_locator))
 
 
 # no modificar
@@ -98,8 +148,9 @@ def retrieve_phone_code(driver) -> str:
             time.sleep(1)
             continue
         if not code:
-            raise Exception("No se encontró el código de confirmación del teléfono.\n"
-                            "Utiliza 'retrieve_phone_code' solo después de haber solicitado el código en tu aplicación.")
+            raise Exception(
+                "No se encontró el código de confirmación del teléfono. "
+                "Asegúrate de haber solicitado el código antes de ejecutar este método.")
         return code
 
 
@@ -118,62 +169,64 @@ class TestUrbanRoutes:
        cls.driver.get(data.urban_routes_url)
        cls.routes_page = UrbanRoutesPage(cls.driver)
 
-
     def test_set_route(self):
-        self.driver.get(data.urban_routes_url)
-        routes_page = UrbanRoutesPage(self.driver)
-        address_from = data.address_from
-        address_to = data.address_to
-        routes_page.set_route(address_from, address_to)
-        assert routes_page.get_from() == address_from
-        assert routes_page.get_to() == address_to
+        self.routes_page.set_from(data.address_from)
+        self.routes_page.set_to(data.address_to)
+        assert self.routes_page.get_from_address() == data.address_from
+        assert self.routes_page.get_to_address() == data.address_to
 
-    def test_pedir_taxi_completo(self):
+    def test_select_plan(self):
         address_from = 'East 2nd Street, 601'
-        self.page.configurar_direccion(address_from)
-        assert address_from == self.page.get_direccion_origen(), f"Dirección de origen incorrecta: {address_from}"
+        self.routes_page.set_from(address_from)
+        assert address_from == self.routes_page.get_from_address(), f"Dirección de origen incorrecta: {address_from}"
 
         address_to = '1300 1st St'
-        self.page.configurar_direccion(address_to)
-        assert address_to == self.page.get_direccion_destino(), f"Dirección de destino incorrecta: {address_to}"
+        self.routes_page.set_to(address_to)
+        assert address_to == self.routes_page.get_to_address(), f"Dirección de destino incorrecta: {address_to}"
 
-        self.page.seleccionar_tarifa_comfort()
-        tarifa_comfort_seleccionada = self.page.obtener_tarifa_seleccionada()
-        assert tarifa_comfort_seleccionada == "Comfort", "La tarifa seleccionada no es 'Comfort'"
-
+    def test_fill_phone_number(self):
         phone_number = '+1 123 123 12 12'
-        self.page.rellenar_telefono(phone_number)
-        assert phone_number == self.page.obtener_telefono(), f"El número de teléfono no es correcto: {phone_number}"
+        self.routes_page.fill_phone_number(phone_number)
+        assert phone_number == self.routes_page.get_phone_number(), f"El número de teléfono no es correcto: {phone_number}"
 
+    def test_fill_card(self):
         card_number = '1234 5678 9100'
         card_code = '111'
-        self.page.agregar_tarjeta(card_number, card_code)
-        assert card_number == self.page.obtener_numero_tarjeta(), f"El número de tarjeta no es correcto: {card_number}"
+        self.routes_page.add_card(card_number, card_code)
+        assert card_number == self.routes_page.get_card_number(), f"El número de tarjeta no es correcto: {card_number}"
 
-        self.driver.get('https://around-v1.es.practicum-services.com/')
         assert "The card link was not activated" in self.driver.page_source, "El enlace de la tarjeta no se activó correctamente."
 
-        self.page.pedir_manta_y_panuelos()
-        assert self.page.is_manta_y_panuelos_pedidos(), "La manta y los pañuelos no fueron solicitados correctamente."
+    def test_order_blanket_and_handkerchiefs(self):
+        self.routes_page.ask_for_a_blanket_and_handkerchiefs()
+        assert self.routes_page.is_blanket_and_handkerchiefs_ordered(), "La manta y los pañuelos no fueron solicitados correctamente."
 
-        self.page.pedir_helados()
-        assert self.page.is_helados_pedidos(), "Los helados no fueron solicitados correctamente."
+    def test_order_2_ice_creams(self):
+        self.routes_page.order_ice_cream()
+        assert self.routes_page.is_ice_cream_orders(), "Los helados no fueron solicitados correctamente."
 
-        message_for_driver = 'Traiga un aperitivo'
-        self.page.message_for_driver(message_for_driver)
-        assert message_for_driver == self.page.obtener_mensaje_para_conductor(), "El mensaje para el conductor no es correcto."
+    def test_comment_for_driver(self):
+        message_for_driver = 'Traer un aperitivo'
+        self.routes_page.message_for_driver(message_for_driver)
+        assert message_for_driver == self.routes_page.get_message_for_driver(), "El mensaje para el conductor no es correcto."
 
-        self.page.esperar_modal_buscando_taxi()
-        modal_buscando_visible = self.page.is_modal_buscando_taxi_visible()
-        assert modal_buscando_visible, "El modal 'Buscando Taxi' no es visible."
+    def test_car_search_model_appears(self):
+        self.routes_page.wait_model_looking_for_taxi()
+        model_searching_visible = self.routes_page.is_model_looking_for_taxi_visible()
+        assert model_searching_visible, "El modal 'Buscando Taxi' no es visible."
 
-        self.page.esperar_informacion_conductor()
-        conductor_info_visible = self.driver.find_element(*UrbanRoutesPage.CONDUCTOR_INFO).is_displayed()
-        assert conductor_info_visible, "La información del conductor no está visible."
+    def test_driver_info_appears(self):
+        self.routes_page.wait_for_driver_information()
+        driver_info_visible = self.driver.find_element(*UrbanRoutesPage.DRIVER_INFO).is_displayed()
 
     def tearDown(self):
-        self.driver.quit()
+        if self.driver:
+            self.driver.quit()
 
+    @classmethod
+    def teardown_class(cls):
+        if cls.driver:
+            cls.driver.quit()
 
     @classmethod
     def teardown_class(cls):
